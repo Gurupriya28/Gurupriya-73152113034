c4c2ec49f3c0161f7764358ecae582cf3e6ae4ab
GuruPriya <gurupriya2728@gmail.com>
2024-07-30 18:42:01 +0530

    angular

--- Angular/MyAngularProject1/src/app/app.component.ts
+++ Angular/MyAngularProject1/src/app/app.component.ts
@@ -7,28 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title="MyAngularProject1";
-  name :string;
-  age : number;
-  email: string;
-  imagePath: string;
+//   name :string;
+//   age : number;
+//   email: string;
+//   imagePath: string;
+//   fname: string;
 
-constructor(){
-  this.name="Priya";
-  this.age=20;
-  this.email="priya@gmail.com";
-  this.imagePath="https://miro.medium.com/v2/resize:fit:720/format:webp/1*lqMtWmTLHxT_6SN4Kjvaog.png"
-}
-chaneName(){
-  this.name="Mouni";
-}
-changeAge(){
-  this.age=21;
-}
-changeEmail(){
-  this.email="mouni@gmail.com";
-}
-changeImage(){
-  this.imagePath="https://miro.medium.com/v2/resize:fit:720/format:webp/1*bMgQ8MhbnQexpqHgIgBJPA.png"
-}
+// constructor(){
+//   this.name="Priya";
+//   this.age=20;
+//   this.email="priya@gmail.com";
+//   this.imagePath="https://miro.medium.com/v2/resize:fit:720/format:webp/1*lqMtWmTLHxT_6SN4Kjvaog.png"
+//   this.fname="";
+// }
+// chaneName(){
+//   this.name="Mouni";
+// }
+// changeAge(){
+//   this.age=21;
+// }
+// changeEmail(){
+//   this.email="mouni@gmail.com";
+// }
+// changeImage(){
+//   this.imagePath="https://miro.medium.com/v2/resize:fit:720/format:webp/1*bMgQ8MhbnQexpqHgIgBJPA.png"
+// }
+   flag:boolean;
+   vehicles:string[];
+   selectedVehicle: string;
+   myStyle:{};
+   MyClass:{};
 
+   constructor(){
+    this.flag=true;
+    this.vehicles=["Twowheelers","ThreeWheeler","FourWheeler"];
+    this.selectedVehicle="";
+    this.myStyle={'width' : '50%' , 'border':'2px solid green', 'border-radius' :'25px'};
+    this.MyClass="MyClass1";
+   }
+   changeFlag(){
+    this.flag=! this.flag;
+   }
+   setSelectedItem(vec: string){
+      this.selectedVehicle=vec;
+   }
+   changeStyle(){
+    this.myStyle={'width' : '40%' , 'border':'2px solid red', 'border-radius' :'25px'};
+   }
 }
--- Angular/MyAngularProject1/src/app/app.module.ts
+++ Angular/MyAngularProject1/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
